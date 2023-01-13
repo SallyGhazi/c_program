2b2a735d5cc3e9e04db8d48b859bbbaac527b970
Sally G. Ibaid <56202060+SallyGhazi@users.noreply.github.com>
2023-01-13 07:15:33 +0200

    Add files via upload

---
+++ whileloop.c
@@ -0,0 +1,14 @@
+/* Write a C program to print all natural numbers from 1 to 10 using loop using while loop  */
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int i = 0 ;
+    while(i<=10) 
+    {
+        printf("%d\t", i);
+        i++;
+    }
+}
+
